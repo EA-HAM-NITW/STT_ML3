b4e97c47986939ca1124d59ef184eecb97f03f98
Ashwin.D.S <astroashwin@outlook.com>
2025-01-11 05:07:05 +0000

    stash

---
+++ setup.sh
@@ -0,0 +1,15 @@
+pip install --upgrade pip
+pip install --upgrade tensorflow tensorflow_io  tensorflow-io speechbrain torchaudio librosa soundfile 
+
+
+mkdir ds
+cd ds
+
+wget https://openslr.elda.org/resources/12/train-clean-100.tar.gz
+wget https://openslr.elda.org/resources/12/dev-clean.tar.gz
+wget https://openslr.elda.org/resources/12/test-clean.tar.gz
+
+tar -xvzf dev-clean.tar.gz
+tar -xvzf test-clean.tar.gz
+tar -xvzf train-clean-100.tar.gz
+
