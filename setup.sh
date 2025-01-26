9e8ee784187df153f46b291bbcb729793456ada3
Ashwin.D.S <astroashwin@outlook.com>
2025-01-26 05:31:16 +0000

    refine the setup file, make a custom venv also

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
+python -m venv venv
+source venv/bin/activate
+
 pip install --upgrade pip
-pip install --upgrade tensorflow tensorflow_io  tensorflow-io speechbrain torchaudio librosa soundfile matplotlib
+pip install --upgrade tensorflow tensorflow_io speechbrain torchaudio librosa soundfile matplotlib
 
-
-mkdir ds
+mkdir -p ds
 cd ds
 
 wget https://openslr.elda.org/resources/12/train-clean-100.tar.gz
@@ -13,3 +15,4 @@ tar -xvzf dev-clean.tar.gz
 tar -xvzf test-clean.tar.gz
 tar -xvzf train-clean-100.tar.gz
 
+cd ..
